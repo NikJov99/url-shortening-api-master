28b09018693ee3b3f4b6334459fb68bce33bfb1b
Nikola Jovanovic <nikolajovanovic.cr99@gmail.com>
2023-06-05 18:04:10 +0200

    add desktop styles

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const toggleMobileMenuButton = document.querySelector(".mobile-menu-button");
 const mobileSubmenu = document.querySelector(".mobile-submenu");
 
-console.log(mobileSubmenu);
-
 const toggleMobileMenu = () => {
   mobileSubmenu.classList.toggle("mobile-submenu-active");
 };
